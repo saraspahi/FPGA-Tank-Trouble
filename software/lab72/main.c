b9f86432e80026f07594da8837665a70e8bd9069
Sara Spahi <sara.spahi@gmail.com>
2021-11-10 23:36:27 -0600

    First commit

---
+++ software/lab72/main.c
@@ -0,0 +1,18 @@
+#include <system.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <alt_types.h>
+#include "text_mode_vga_color.h"
+
+
+int main() {
+	//while(1){
+	text_mode_vga_color();
+		//for(int i=0; i<1000000; i++);
+	//}
+
+	return 0;
+}
+
+
---
+++ software/lab72/obj/default/text_mode_vga_color.d
@@ -0,0 +1,11 @@
+obj/default/text_mode_vga_color.o: text_mode_vga_color.c \
+ ../lab72_bsp/system.h ../lab72_bsp/linker.h \
+ ../lab72_bsp//HAL/inc/alt_types.h text_mode_vga_color.h
+
+../lab72_bsp/system.h:
+
+../lab72_bsp/linker.h:
+
+../lab72_bsp//HAL/inc/alt_types.h:
+
+text_mode_vga_color.h:
