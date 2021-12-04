9009809b901c8bc877db335a2c62a7ac98396b09
Blerim Abdullai <bl3rimabdullai@gmail.com>
2021-12-03 18:27:59 -0600

    maze writes

--- software/tank_trouble_bsp/public.mk
+++ software/tank_trouble_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0xe8
 ELF_PATCH_FLAG  += --sidp 0xe8
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1638508884
-SOPC_SYSID_FLAG += --timestamp=1638508884
-ELF_PATCH_FLAG  += --timestamp 1638508884
+# setting SOPC_TIMESTAMP is 1638576855
+SOPC_SYSID_FLAG += --timestamp=1638576855
+ELF_PATCH_FLAG  += --timestamp 1638576855
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
--- software/tank_trouble_bsp/system.h
+++ software/tank_trouble_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2_0' in SOPC Builder design 'lab62_soc'
  * SOPC Builder design path: ../../lab62_soc.sopcinfo
  *
- * Generated: Thu Dec 02 23:24:13 CST 2021
+ * Generated: Fri Dec 03 18:17:50 CST 2021
  */
 
 /*
@@ -457,7 +457,7 @@
 #define SYSID_QSYS_0_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_0_NAME "/dev/sysid_qsys_0"
 #define SYSID_QSYS_0_SPAN 8
-#define SYSID_QSYS_0_TIMESTAMP 1638508884
+#define SYSID_QSYS_0_TIMESTAMP 1638576855
 #define SYSID_QSYS_0_TYPE "altera_avalon_sysid_qsys"
 
 
